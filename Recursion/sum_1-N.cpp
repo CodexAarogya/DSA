1519575e24437b87f7593885e4ac05e363940e5a
Aarogya Parajuli <aarogyaparajuli07@gmail.com>
2025-06-19 15:44:19 +0545

    Printing sum of N numbers using recursion

--- Recursion/sum_1-N.cpp
+++ Recursion/sum_1-N.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int printSum(int n){
-    if(n < 1){
+int printSum(int n)
+{
+    if (n < 1)
+    {
         return 0;
     }
-    return n + printSum(n-1);
+    return n + printSum(n - 1);
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the value of N: ";
-    cin>> n;
+    cout << "Enter the value of N: ";
+    cin >> n;
     int total = printSum(n);
-    cout<<"Sum: "<<total;
+    cout << "Sum: " << total;
 }
