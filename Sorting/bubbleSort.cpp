b0ecc8ec32e1be454f2e04dd5d83c9cba4d69a51
Aarogya Parajuli <aarogyaparajuli07@gmail.com>
2025-07-27 21:55:52 +0545

    Updated bubble sort

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+void display(int arr[], int size = 5)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     int arr[5] = {10, 12, -3, 0, 99};
@@ -13,8 +22,13 @@ int main()
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+
+            cout << "i = " << i << ": ";
+            display(arr);
         }
+        cout << endl;
     }
+    cout << "Sorted: ";
     for (int i = 0; i < 5; i++)
     {
         cout << arr[i] << " ";
