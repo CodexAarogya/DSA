0063468338e64c4b59849fe7f01b430f9d075280
Aarogya Parajuli <aarogyaparajuli07@gmail.com>
2025-07-31 10:59:56 +0545

    Merge sort--Incomplete code---

---
+++ Sorting/mergeSort.cpp
@@ -0,0 +1,25 @@
+
+    // !!!!!! INCOMPLETE CODE  ..... !!!!!!
+
+// #include <iostream>
+// using namespace std;
+// int low = 1;
+// int high = 5;
+// void divide(int arr[],int low, int high)
+// {
+//     int mid = (low + high) * 0.5;
+//     if (low < high)
+//     {   
+
+//         divide(arr[mid],low, mid);
+//         divide(mid + 1, high);
+//     }
+// }
+// void conquer()
+// {
+// }
+// int main()
+// {
+//     int arr[5] = {10, 12, -3, 0, 99};
+//     divide(arr,low,high);
+// }
